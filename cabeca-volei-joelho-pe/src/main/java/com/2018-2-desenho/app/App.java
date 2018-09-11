2abc98c75d905b2167f7396f0ac2346dfc90ae2a
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2018-09-10 22:04:54 -0300

    Added maven project

---
+++ cabeca-volei-joelho-pe/src/main/java/com/2018-2-desenho/app/App.java
@@ -0,0 +1,13 @@
+package com.2018-2-desenho.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
