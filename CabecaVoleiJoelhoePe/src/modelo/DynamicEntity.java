45bd5b1025d83242589d35002c8177ee15c48307
Lucas Pereira <lpalucas.10@gmail.com>
2018-11-03 19:13:34 -0300

    Signed-off-by: Lucas Pereira <lpalucas.10@gmail.com> Co-Authored-by: Lucas Siqueira <lucassiqzro@gmail.com> Co-Authored-by: Paulo Victor <pv.paulovictor.vp@gmail.com>

--- CabecaVoleiJoelhoePe/src/modelo/DynamicEntity.java
+++ CabecaVoleiJoelhoePe/src/modelo/DynamicEntity.java
@@ -1,6 +1,5 @@
 package modelo;
 
-import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
 
 public abstract class DynamicEntity extends AbstractEntity{
@@ -38,6 +37,49 @@ public abstract class DynamicEntity extends AbstractEntity{
         } else if(this.speed.y > this.maxSpeed.y) {
             this.speed.y = this.maxSpeed.y;
 	}
+        
+        if (this.speed.y < 0) {
+            if (this.collidingEntities[COLLIDING_TOP] != null) {
+		this.position.y = this.collidingEntities[COLLIDING_TOP].position.y + this.collidingEntities[COLLIDING_TOP].position.height;
+		this.speed.y = 0;
+		this.acceleration.y = 0;
+            }
+	} else if (this.speed.y > 0) {
+            if (this.collidingEntities[COLLIDING_DOWN] != null) {
+		this.position.y = this.collidingEntities[COLLIDING_DOWN].position.y - this.position.height + 1;
+		this.speed.y = 0;
+		this.acceleration.y = 0;
+            }
+	}
+
+	if(this.speed.x < 0) {
+            if(this.collidingEntities[COLLIDING_LEFT] != null) {
+		this.position.x = this.collidingEntities[COLLIDING_LEFT].position.x + this.collidingEntities[COLLIDING_LEFT].position.width - 1;
+		this.speed.x = 0;
+		this.acceleration.x = 0;
+            } else {
+		this.speed.x += this.friction;
+		if(this.speed.x > 0) {
+                    this.speed.x = 0;
+		}
+            }
+	} else if(this.speed.x > 0) {
+            if(this.collidingEntities[COLLIDING_RIGHT] != null) {
+		this.position.x = this.collidingEntities[COLLIDING_RIGHT].position.x - this.position.width + 1;
+		this.speed.x = 0;
+		this.acceleration.x = 0;
+            } else {
+		this.speed.x -= this.friction;
+		if (this.speed.x < 0) {
+                    this.speed.x = 0;
+		}
+            }
+	}
+	
+        this.position.x += this.speed.x;
+        this.position.y += this.speed.y;
+	this.acceleration.x = 0;
+	this.acceleration.y = 0;
     }
     
 }
